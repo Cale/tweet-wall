23b79224d32ec4c0c5f63f51898b178b87bff775
Cale Mooth <cale@midnightcheese.com>
2018-08-26 18:33:38 -0500

    Link users and hashtags.

--- js/tweet-wall.js
+++ js/tweet-wall.js
@@ -18,25 +18,44 @@ $( document ).ready(function() {
             tweetText = tweet.full_text;
         }
         var urls;
+        var users;
+        var hashtags;
         var tweetUrlRemoved = tweetText
-        var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-        var regex = new RegExp(expression);
-        urls = tweetText.match(regex);
-        console.log(urls);
+        var urlExpression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+        var usersExpression = /(^|[^@\w])@(\w{1,15})\b/g;
+        var hashtagExpression = /(\S*#\[[^\]]+\])|(\S*#\S+)/gi;
+        var urlRegex = new RegExp(urlExpression);
+        var usersRegex = new RegExp(usersExpression);
+        var hashtagRegex = new RegExp(hashtagExpression);
+        urls = tweetText.match(urlRegex);
+        users = tweetText.match(usersRegex);
+        hashtags = tweetText.match(hashtagRegex);
         if (urls) {
             $.each(urls, function( i, url ) {
                 console.log(i+' '+url);
-                tweetUrlRemoved = tweetUrlRemoved.replace(url,'');
-                if ( (i+1) == urls.length ) {
-                    $.each(urls, function( i, url ) {
-                        tweetUrlRemoved = tweetUrlRemoved+' <a href="'+url+'">'+url+'</a>';
-                    });
-                }
+                tweetUrlRemoved = tweetUrlRemoved.replace(url,'<a href="'+url+'" target="_blank">'+url+'</a>');
+            });
+        }
+        if (users) {
+            $.each(users, function( i, user ) {
+                console.log(i+' '+user);
+                nospace = user.trim();
+                noat = nospace.replace("@",'');
+                noat = nospace.replace(".",'');
+                tweetUrlRemoved = tweetUrlRemoved.replace(user,' <a href="https://twitter.com/'+noat+'" target="_blank">'+nospace+'</a>');
+            });
+        }
+        if (hashtags) {
+            $.each(hashtags, function( i, tag ) {
+                console.log(i+' '+tag);
+                nospace = tag.trim();
+                nohash = nospace.replace("#",'');
+                tweetUrlRemoved = tweetUrlRemoved.replace(tag,' <a href="https://twitter.com/hashtag/'+nohash+'?src=hash" target="_blank">'+nospace+'</a>');
             });
         }
         console.log(tweetUrlRemoved);
         $( "p" ).fadeOut( "fast", function() {
-            $(".user").html('<a href="https://twitter.com/'+tweet.user.screen_name+'">@'+tweet.user.screen_name+'</a>');
+            $(".user").html('<a href="https://twitter.com/'+tweet.user.screen_name+'" target="_blank">@'+tweet.user.screen_name+'</a>');
             $(".tweet").html(tweetUrlRemoved);
             $("p").fadeIn( "fast", function() {
                 if ( (i+1) == tweetCount ) {
